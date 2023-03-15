d194526f3bd0156cb80005d1740e34a3417d0b73
scyonggg <scyongg@inha.edu>
2023-03-15 13:39:29 +0900

    minor

--- main.py
+++ main.py
@@ -40,7 +40,8 @@ def validation(model, val_loader, loss_fn):
 
             losses.append(loss)
             acc1_, acc5_ = accuracy(outputs, gt, topk=(1, 5))
-            print(f'acc1_ : {acc1_}, acc5_ : {acc5_}')
+            if acc1_[0] != 0.:
+                print(f'acc1_ : {acc1_}, acc5_ : {acc5_}')
             acc1 += acc1_[0]
             acc5 += acc5_[0]
 
